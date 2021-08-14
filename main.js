a7315f5e0f4d4346d0bfa044b0b64a421783a1e7
KavyaBhartia <77235966+KavyaBhartia@users.noreply.github.com>
2021-08-14 20:25:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Prediction = "";
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    })
+}
+console.log("ml5 version: ", ml5.version);
+classifier = imageClassifier('https://teachablemachine.withgoogle.com/models/vGR6mOPLm/model.json', modelLoaded);
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
